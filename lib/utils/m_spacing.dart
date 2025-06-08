29d241756dd38a4cd57ab2ecad1dcb8d0dee7d91
Fernando Bráulio <fernandobraulio@outlook.com.br>
2025-06-08 12:13:18 +0100

    m_spacing

---
+++ lib/utils/m_spacing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// Espaçamento vertical com altura customizável (padrão: 12.0)
+SizedBox mHeight([double height = 12.0]) {
+  return SizedBox(height: height);
+}
+
+/// Espaçamento horizontal com largura customizável (padrão: 12.0)
+SizedBox mWidth([double width = 12.0]) {
+  return SizedBox(width: width);
+}
