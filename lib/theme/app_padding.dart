ea34e3650619e8f8a42a0331b410f1792685c0ca
Fernando Bráulio <fernandobraulio@outlook.com.br>
2025-12-02 21:47:56 +0000

    Update app_padding.dart

--- lib/theme/app_padding.dart
+++ lib/theme/app_padding.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppPadding {
   static const EdgeInsetsGeometry primary = EdgeInsets.only(top: 20.0, left: 16.0, right: 16.0);
+  static const EdgeInsetsGeometry defaultPadding = EdgeInsets.all(12.0);
 }
